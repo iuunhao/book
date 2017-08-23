6d41d322c20f79e523b84bbfeb36e78ba28f3ca7
iuunhao <html.5@foxmail.com>
2017-08-23 21:13:48 +0800

    update

--- server/base/connect.js
+++ server/base/connect.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27017/book');
+
+var env = process.env.NODE_ENV || 'development';
+var dbUrl = 'mongodb://iuunhao_book:iuunhao/127.0.0.1:27017/book'
+if(env=== 'development'){
+  dbUrl='mongodb://iuunhao_book:iuunhao/127.0.0.1:27017/book'
+}
+
+mongoose.connect(dbUrl);
 
 mongoose.connection.on('error', function() {
   console.log('数据库连接失败！')
